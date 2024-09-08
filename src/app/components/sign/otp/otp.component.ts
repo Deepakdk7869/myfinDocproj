a2398caf2456b72b147833511957509c89c4af12
Deepak <pahandeepak7869@gmail.com>
2024-09-08 13:32:12 +0530

    folder set successful

--- src/app/components/sign/otp/otp.component.ts
+++ src/app/components/sign/otp/otp.component.ts
@@ -8,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrl: './otp.component.css'
 })
 export class OtpComponent {
+  time1!:string;
+  time2!:string;
+  interval: any;
+  constructor(){}
+  ngOnInit(){
 
+  }
+  startTimer(time:string){}
+  //   let sec = 30;
+  //   let minut = 0;
+  //   this.interval = setTimeout(() => {
+  //     if(sec != 0){
+  //       sec--
+  //     }else if(sec == 0){
+  //     clearInterval(this.interval)
+  //     }
+  //   }, 1000);
+  // }
 }
--- src/app/components/sign/sign.component.ts
+++ src/app/components/sign/sign.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { MobileinputComponent } from './mobileinput/mobileinput.component';
+import { OtpComponent } from './otp/otp.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sign',
   standalone: true,
-  imports: [],
+  imports: [MobileinputComponent,OtpComponent, CommonModule],
   templateUrl: './sign.component.html',
   styleUrl: './sign.component.css'
 })
 export class SignComponent {
 
+  mobileinput!:string  ;
+  ngOnInit(){
+    this.mobileinput = ''; 
+  }
 }
