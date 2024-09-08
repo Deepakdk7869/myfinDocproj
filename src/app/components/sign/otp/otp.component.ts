53c30cf2aac0cc3cc2d53f4cd3f7f5e1be6bd50a
Deepak <pahandeepak7869@gmail.com>
2024-09-08 16:32:01 +0530

    otp timer fuction written

--- src/app/components/sign/otp/otp.component.ts
+++ src/app/components/sign/otp/otp.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { min } from 'rxjs';
 
 @Component({
   selector: 'app-otp',
@@ -8,22 +9,28 @@ import { Component } from '@angular/core';
   styleUrl: './otp.component.css'
 })
 export class OtpComponent {
-  time1!:string;
-  time2!:string;
-  interval: any;
+  Intervals:any = {};
+  timerValues:any = { 1: 12, 2: 12 }; 
+  isRunning:any = {1:false, 2:false}
   constructor(){}
   ngOnInit(){
-
+      this.startTimer(1);
+      this.startTimer(2);
+    }
+  startTimer(timerId:any){
+    if(this.isRunning[timerId]){
+      return
+    }
+    this.isRunning[timerId] = true;
+    this.Intervals[timerId] = setInterval(() => {
+      this.timerValues[timerId]--
+      let element = document.getElementById(`${timerId}`) as HTMLElement 
+      element.innerText = `00 : ${this.timerValues[timerId].toString().padStart(2,'0')}`
+      if(this.timerValues[timerId] == 0){
+        clearInterval(this.Intervals[timerId])
+        this.isRunning[timerId] = false;
+        this.timerValues[timerId] = 12;
+      }
+    }, 1000);
   }
-  startTimer(time:string){}
-  //   let sec = 30;
-  //   let minut = 0;
-  //   this.interval = setTimeout(() => {
-  //     if(sec != 0){
-  //       sec--
-  //     }else if(sec == 0){
-  //     clearInterval(this.interval)
-  //     }
-  //   }, 1000);
-  // }
 }
