1618946bd88bcf5ca4446b0ed9462ead8b311c7d
Deepak <pahandeepak7869@gmail.com>
2024-09-08 17:01:39 +0530

    add validator to stop non numeric value

--- src/app/components/sign/mobileinput/mobileinput.component.ts
+++ src/app/components/sign/mobileinput/mobileinput.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-mobileinput',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './mobileinput.component.html',
   styleUrl: './mobileinput.component.css'
 })
-export class MobileinputComponent {
-
+export class MobileinputComponent implements OnInit {
+  signInForm ! : FormGroup;
+  constructor(private fb: FormBuilder){}
+  ngOnInit(): void {
+    this.signInForm = this.fb.group({
+      mobileno:['',[Validators.required,Validators.minLength(10),this.onlyNumber()]],
+      email:['',[Validators.required,Validators.email]]
+    })
+  }
+  submitForm(){
+    if(this.signInForm.valid){  
+      console.log(this.signInForm.value)
+    }
+  }
+  onlyNumber():ValidatorFn{
+    return (control:AbstractControl)=>{
+      let value = control.value.trim();
+      if(!(/^[0-9]+$/.test(value))){
+        return {error: 'Only number allowed'}
+      }
+      return null
+    }
+  }
 }
