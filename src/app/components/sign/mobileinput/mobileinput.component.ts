f104cca79a7b0dd037c20b67e993746c7b1d69f3
Deepak <pahandeepak7869@gmail.com>
2024-12-30 23:52:49 +0530

    changes from the first branch

--- src/app/components/sign/mobileinput/mobileinput.component.ts
+++ src/app/components/sign/mobileinput/mobileinput.component.ts
@@ -26,7 +26,7 @@ export class MobileinputComponent implements OnInit {
   onlyNumber():ValidatorFn{
     return (control:AbstractControl)=>{
       let value = control.value.trim();
-      if(!(/^[0-9]+$/.test(value))){
+      if(value && !(/^[0-9]+$/.test(value))){
         return {error: 'Only number allowed'}
       }
       return null
